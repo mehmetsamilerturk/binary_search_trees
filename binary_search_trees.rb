abae45c4ef07e0123d1e11039abaf6f52958cf48
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2022-01-26 00:33:08 +0300

    insert method done

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -1,6 +1,6 @@
 class Node
   attr_accessor :data, :left, :right
-  
+
   def initialize(data)
     @data = data
     @left = nil
@@ -18,30 +18,40 @@ class Tree
 
   def build_tree(array, start = 0, end_array = array.length - 1)
     return nil if start > end_array
+
     mid = ((start + end_array) / 2)
     root_node = Node.new(array[mid])
     root_node.left = build_tree(array, start, mid - 1)
     root_node.right = build_tree(array, mid + 1, end_array)
-    return root_node
+    root_node
   end
 
-  def call_each(current)    
+  def call_each(current = @root)
     return nil if current.nil?
-    
+
     puts current.data
     call_each(current.left)
     call_each(current.right)
   end
 
-  def insert(value)
-    return nil if current.nil?
+  def insert(value, current = @root)
+    return Node.new(value) if current.nil?
 
-    
+    if value > current.data
+      current.right = insert(value, current.right)
+    else
+      current.left = insert(value, current.left)
+    end
+
+    current
   end
 
-  def delete(value)
-  end
+  def delete(value); end
 end
 
 numbers = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
 tree = Tree.new(numbers)
+tree.insert(70)
+tree.call_each
+p tree
