7c0ffb07c100b316cdb02097bf2ba226cafa0b46
Mehmet Samil Erturk <mehmetsamilerturk@outlook.com>
2022-01-26 16:59:15 +0300

    added #delete and #inorder_traversal

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -16,16 +16,6 @@ class Tree
     @root = build_tree(@array)
   end
 
-  def build_tree(array, start = 0, end_array = array.length - 1)
-    return nil if start > end_array
-
-    mid = ((start + end_array) / 2)
-    root_node = Node.new(array[mid])
-    root_node.left = build_tree(array, start, mid - 1)
-    root_node.right = build_tree(array, mid + 1, end_array)
-    root_node
-  end
-
   def call_each(current = @root)
     return nil if current.nil?
 
@@ -46,12 +36,66 @@ class Tree
     current
   end
 
-  def delete(value); end
+  def delete(value, current = @root)
+    return current if current.nil?
+
+    if value < current.data
+      current.left = delete(value, current.left)
+    elsif value > current.data
+      current.right = delete(value, current.right)
+    else
+      if current.left.nil?
+        return current.right
+      elsif current.right.nil?
+        return current.left
+      end
+
+      current.data = min_value(current.right)
+      current.right = delete(current.data, current.right)
+    end
+
+    current
+  end
+
+  def inorder_traversal(current = @root)
+    unless current.nil?
+      inorder_traversal(current.left)
+      print "#{current.data} -> "
+      inorder_traversal(current.right)
+    end
+  end
+
+  private
+
+  def min_value(current = @root)
+    minv = current.data
+
+    until current.left.nil?
+      minv = current.left.data
+      current = current.left
+    end
+
+    minv
+  end
+
+  def build_tree(array, start = 0, end_array = array.length - 1)
+    return nil if start > end_array
+
+    mid = ((start + end_array) / 2)
+    root_node = Node.new(array[mid])
+    root_node.left = build_tree(array, start, mid - 1)
+    root_node.right = build_tree(array, mid + 1, end_array)
+    root_node
+  end
 end
 
 numbers = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 
 tree = Tree.new(numbers)
 tree.insert(70)
-tree.call_each
-p tree
+tree.delete(3)
+tree.delete(23)
+tree.insert(25)
+tree.delete(8)
+# tree.call_each
+tree.inorder_traversal
